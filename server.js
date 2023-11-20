6affd9b7e4687b4a6c48b430b23fc97d80086cd3
Mr Wait <brennanmitchell1192@gmail.com>
2023-11-19 20:01:07 -0600

    Server endpoints created. Middleware added.

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
-const { readFile, writeFile } = require('fs').promises;
+const { readFile, writeFile } = require('fs').promises
 const express = require("express");
-const path = require('path')
+const path = require('path');
 
 const app = express()
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'notes.html'));
+});
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
 
 
 
